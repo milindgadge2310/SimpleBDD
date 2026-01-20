9dca0ee42e5f03d54ba940aa62faebb32968742d
padma555 <padma5.sai@gmail.com>
2026-01-20 12:17:27 +0530

    code updated

--- src/main/java/BaseLayer/BaseClass.java
+++ src/main/java/BaseLayer/BaseClass.java
@@ -5,6 +5,12 @@ public class BaseClass {
 	public static void main(String[] args) {
 		System.out.println("I have Created Framework");
 	}
+	
+	public void update(){
+		System.out.println("I have updated Framework");
+	}
+	
+	
 	}
 
 
